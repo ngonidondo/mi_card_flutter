bc2799a5c7b0ce4787e421e04545b9d3d5d040ca
Ngoni <ngonidondo@gmail.com>
2020-04-30 08:06:24 -0400

    learning about rows and columns and positioning and whatnot

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            verticalDirection: VerticalDirection.up,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                color: Colors.red[100],
+                child: Text("Container 1"),
+                height: 100.0,
+                width: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+                padding: EdgeInsets.all(12.0),
+              ),
+              Container(
+                color: Colors.red[200],
+                child: Text("Container 2"),
+                height: 100.0,
+                width: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+                padding: EdgeInsets.all(12.0),
+              ),
+              Container(
+                color: Colors.red[300],
+                child: Text("Container 3"),
+                height: 100.0,
+                width: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+                padding: EdgeInsets.all(12.0),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
